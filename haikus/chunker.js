2c96bb16135cdbfa4a9ebdec885efc32790f3e3f
Jose <jlabella@emeal.nttdata.com>
2025-04-04 13:59:40 +0200

    chunk haikus

---
+++ haikus/chunker.js
@@ -0,0 +1,33 @@
+import fs from "fs";
+
+const CHUNK_SIZE = 20;
+
+fs.readFile("./haikus.json", { encoding: "utf8" }, (err, data) => {
+  const haikus = JSON.parse(data).sort((a, b) => b.id - a.id);
+  const totalChunks = Math.ceil(haikus.length / CHUNK_SIZE);
+  fs.writeFile(
+    "./director.json",
+    JSON.stringify({
+      totalChunks,
+      chuckSize: CHUNK_SIZE,
+    }),
+    (err, data) => {
+      err && console.log(err);
+    }
+  );
+  for (let i = 0; i < totalChunks; i++) {
+    fs.writeFile(
+      `./chuck_${i}.json`,
+      JSON.stringify(
+        haikus.slice(i * CHUNK_SIZE, (i + 1) * CHUNK_SIZE),
+        null,
+        2
+      ),
+      (err) => {
+        if (err) {
+          console.log("Error with chunk ", i);
+        }
+      }
+    );
+  }
+});
