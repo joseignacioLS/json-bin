594c14c5f8fbdd033b563326815b19cdf9ae676d
Jose <jlabella@emeal.nttdata.com>
2025-04-04 14:54:01 +0200

    add collections

--- haikus/chunker.js
+++ haikus/chunker.js
@@ -1,15 +1,21 @@
 import fs from "fs";
 
 const CHUNK_SIZE = 20;
-
+const HIDDEN_COLLECTIONS = ["5-7-5", "3-5-3"];
 fs.readFile("./haikus.json", { encoding: "utf8" }, (err, data) => {
   const haikus = JSON.parse(data).sort((a, b) => b.id - a.id);
+  const collections = Array.from(
+    new Set(
+      haikus.reduce((tags, { tags: newTags }) => [...tags, ...newTags], [])
+    )
+  ).filter((tag) => !HIDDEN_COLLECTIONS.includes(tag));
   const totalChunks = Math.ceil(haikus.length / CHUNK_SIZE);
   fs.writeFile(
     "./director.json",
     JSON.stringify({
       totalChunks,
       chunkSize: CHUNK_SIZE,
+      collections,
     }),
     (err, data) => {
       err && console.log(err);
